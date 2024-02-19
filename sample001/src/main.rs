1cd2d764b869f52372047aab026ab12b2dfc8846
Sławomir Bryś <slawomir.brys@sonova.com>
2024-02-19 11:32:30 +0100

    Sample001

---
+++ sample001/src/main.rs
@@ -0,0 +1,33 @@
+fn main() {
+    println!("Hello, world!");
+
+    println!("{} days", 31);
+    println!("{} text", "test");
+    println!("{} char", 'a');
+
+    println!("{0}, this is {1}. {1}, this is {0}", "Alice", "Bob");
+
+    println!("{subject} {verb} {object}",
+             object="the lazy dog",
+             subject="the quick brown fox",
+             verb="jumps over");
+
+    let email = "test@test.test";
+    println!("My email is {email}");
+
+    println!("Base 10:               {}",   69420); // 69420
+    println!("Base 2 (binary):       {:b}", 69420); // 10000111100101100
+    println!("Base 8 (octal):        {:o}", 69420); // 207454
+    println!("Base 16 (hexadecimal): {:x}", 69420); // 10f2c
+    println!("Base 16 (hexadecimal): {:X}", 69420); // 10F2C
+    println!("Base 255 (hex):        {:p}", email);   // ff
+
+    println!("{number:>5}", number=1);
+    println!("{number:0>5}", number=1); // 00001
+    println!("{number:0<5}", number=1); // 10000
+    println!("{number:*<5}", number=1); // 1****
+
+    println!("{number:0>width$}", number=1, width=5);
+
+    eprintln!("Some error occurred");
+}
